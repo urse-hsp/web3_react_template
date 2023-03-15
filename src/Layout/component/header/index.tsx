c201551d8819f51e919a882cfb964db772e19a40
p <email>
2023-03-15 14:25:45 +0800

    feat: init

--- src/Layout/component/header/index.tsx
+++ src/Layout/component/header/index.tsx
@@ -1,9 +1,14 @@
 import React, { useState, memo } from 'react';
-import './index.scss';
-import { Space } from 'antd';
+import { Space, Button } from 'antd';
 import Language from '../language';
-import UserConnectBox from '../UserConnectBox';
 import { useNavigate } from 'react-router-dom';
+import { useWeb3Provider } from '@/models/Web3ReactProvider';
+import config from '@/config';
+import { useTranslation } from 'react-i18next';
+import WalletInfo from '../walletInfo';
+import IconFont from '@/components/IconFont';
+import Modal from '@/components/Modal';
+import './index.scss';
 
 interface IndexType {
   isVisible?: boolean;
@@ -13,39 +18,63 @@ interface IndexType {
 const Leftview = memo(() => {
   return (
     <Space className="cursor">
-      {/* <img src={url} alt="" style={{ width: '30px', height: '30px' }} /> */}
+      <img
+        src={
+          'https://img2.baidu.com/it/u=1035356506,3713698341&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1678986000&t=9ac49d503ad7245757d4e57664354f1f'
+        }
+        alt=""
+        style={{ width: '30px', height: '30px' }}
+      />
       <span style={{ fontWeight: 'bold' }}></span>
     </Space>
   );
 });
 
-interface rightviewType {
-  onClick: () => void;
-}
-const Rightview = memo(({ onClick }: rightviewType) => {
-  const navigate = useNavigate();
-  const menuSelect = (e: any) => {
-    navigate(e.key);
-  };
+const UserConnectBox: React.FC = () => {
+  const { t } = useTranslation();
+  const { connect, active, loading, account } = useWeb3Provider();
+
   return (
-    <Space>
-      <Language />
-      <UserConnectBox />
-    </Space>
+    <>
+      <>
+        {active && (
+          <WalletInfo>
+            <div className={'user_wallet flex-item-center cursor'}>
+              {account}
+              <IconFont type="icon-a-Frame427318819" />
+            </div>
+          </WalletInfo>
+        )}
+        {!active && (
+          <Button
+            className={'cursor connect_btn'}
+            onClick={() =>
+              connect(config.BASE_NETWORK_ID, config.BASE_WALLET_TYPE)
+            }
+            loading={loading}
+          >
+            {t('Connect Wallet')}
+          </Button>
+        )}
+      </>
+      <Modal isVisible={false} />
+    </>
   );
-});
+};
+const UserConnectView = memo(UserConnectBox);
 
 const Index: React.FC<IndexType> = (props) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="root-header flex-between">
-      <Leftview />
-      <Rightview
-        onClick={() => {
-          setOpen(!open);
-        }}
-      />
+    <nav className="root-header">
+      <div className="root-header-main flex-between">
+        <Leftview />
+        <Space>
+          <Language />
+          <UserConnectView />
+        </Space>
+      </div>
     </nav>
   );
 };
